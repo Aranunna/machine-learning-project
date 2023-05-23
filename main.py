a2471f3d590a2edacbaaafe6842ccec4216b79e5
Aranunna <62034932+Aranunna@users.noreply.github.com>
2023-05-22 18:28:49 -0700

    Graphs and User Input
    Added the monthly comparison between models graph as well as the kWh/month total output. Also included a part to have the user input a date and have the model predict.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 
 #  Converts the CSV into a Pandas DataFrame
@@ -11,13 +11,21 @@ data['date'] = pd.to_datetime(data['date'])
 
 #  Creates a copy of the data set with only date and kwh production
 df_dkwh = data.drop(columns=['name', 'id', 'address', 'public_url', 'installationDate', 'uid'])
+
+#  Making a monthly view of power output
+mdf = df_dkwh.resample(rule='M', on='date')['kWh'].sum()
+monthlyx = mdf.index
+monthlyy = mdf
+plt.bar(monthlyx, monthlyy,width=25)
+plt.show()
+
+#  Continuing to prepare the data
 df_dkwh = df_dkwh.set_index('date')
 df_dkwh = df_dkwh.groupby([df_dkwh.index.date])['kWh'].sum()
 df_dkwh = df_dkwh.reset_index()
 df_dkwh = df_dkwh.rename(columns={'index': 'date'})
 features = ["year", "month", "day"]
 df_dkwh[features] = df_dkwh.apply(lambda row: pd.Series({"year":row.date.year, "month":row.date.month, "day":row.date.day}), axis=1)
-print(df_dkwh)
 
 #  Splits the testing and training sets before and after target date
 split_date = pd.Timestamp('2021-01-01')
@@ -35,6 +43,7 @@ Ytrain = dftrain['kWh']
 #  Reshaping data
 Ytrain = Ytrain.array.reshape((-1, 1))
 Ytest = Ytest.array.reshape((-1, 1))
+print(Ytrain)
 
 #  Instantiating the models
 modelLR = LinearRegression()
@@ -46,18 +55,32 @@ modelDTR.fit(Xtrain, Ytrain)
 LRpred = modelLR.predict(Xtest)
 DTRpred = modelDTR.predict(Xtest)
 
-#Testing
-print(mean_squared_error(LRpred, Ytest))
-print(mean_squared_error(DTRpred, Ytest))
+#  Testing
+#  Plotting the expected results alongside what the two models returned
+modeltest = pd.DataFrame(testplot)
+modeltest['Test kWh'] = Ytest
+modeltest['LR'] = LRpred
+modeltest['DTR'] = DTRpred
+modeltest['date'] = pd.to_datetime(modeltest['date'])
+modeltest = modeltest.resample(rule='M', on='date')['Test kWh', 'LR', 'DTR'].sum()
+plt.bar(modeltest.index - pd.Timedelta(20, unit='d'), modeltest['Test kWh'], label="Test kWh", color='black', width=10)
+plt.bar(modeltest.index - pd.Timedelta(10, unit='d'), modeltest['LR'], label="LR", color='blue', width=10)
+plt.bar(modeltest.index, modeltest['DTR'], label="DTR", color='red', width=10)
+plt.legend()
+plt.show()
+print(mean_squared_error(modeltest['LR'], modeltest['Test kWh']))
+print(mean_squared_error(modeltest['DTR'], modeltest['Test kWh']))
+print(r2_score(modeltest['LR'], modeltest['Test kWh']))
+print(r2_score(modeltest['DTR'], modeltest['Test kWh']))
 
-#  Plotting
+#  Plotting the daily comparison
 plt.title('Power Output')
 plt.xlabel('Date')
 plt.ylabel('kWh')
-
 plt.scatter(trainplot, Ytrain, color='black')
 plt.plot(testplot, LRpred, color='blue')  # Linear Regression plot
 plt.plot(testplot, DTRpred, color='orange')  # DT Regressor plot
 plt.scatter(testplot, Ytest, color='red')  # Testing reference plot
-
 plt.show()
+
+#  User interactivity
